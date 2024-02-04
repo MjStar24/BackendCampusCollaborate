64a4baa64b1a9948a50c298aeb827c7f0a94b19a
MjStar <starmj24@gmail.com>
2024-02-04 14:31:14 +0530

    made courseReviewModel

---
+++ models/courseReviewModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const courseReviewSchema=new mongoose.Schema({
+    courseCode:{type:String,required:true},
+    courseName:{type:String,requied:true},
+    professor:{type:String,required:true},
+    description:{type:String,default:""},
+    rating:{type:Number,required:true},
+    comments:[{
+        user:{type:Schema.Types.ObjectId,ref:"User"},
+        comment:{type:String,default:""}
+    }]
+})
+
+const CourseReview=mongoose.model("CourseReview",courseReviewSchema);
+export default CourseReview;
